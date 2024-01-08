bd9e10f0b580eba24cfe2b99a4d36cf6a4b18423
marzieh <marziehmonshizade@yahoo.com>
2024-01-09 02:33:42 +0330

    Added styling for scrollbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ import Navbar from "@/components/navbar";
 
 export default function Home() {
   return (
-    <main className="bg-zinc-800 text-zinc-500 text-lg snap-y snap-mandatory h-screen overflow-scroll overflow-x-hidden z-0">
+    <main className="bg-zinc-800 text-zinc-500 text-lg snap-y snap-mandatory overflow-y-scroll overflow-x-hidden h-screen scrollbar scrollbar-thumb-orange-500 scrollbar-track-gray-400/20 z-0">
       {/* navbar */}
       <Navbar />
       {/* hero */}
@@ -20,7 +20,7 @@ export default function Home() {
         <AboutMe />
       </section>
       {/* skills */}
-      <section id="skills" className="snap-center">
+      <section id="skills" className="snap-start">
         <Skills />
       </section>
       {/* projects */}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,8 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('tailwind-scrollbar'),
+  ],
 }
 export default config

49007b442d15dffc19ab5cae7ec2f86ad236bc73
marzieh <marziehmonshizade@yahoo.com>
2024-01-09 14:25:09 +0330

    coding projects section

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,14 +1,70 @@
-import Project from "./project";
-import ScrollToTopBtn from "./scrollToTopBtn";
+import { motion } from "framer-motion";
+import { Swiper, SwiperSlide } from "swiper/react";
 
+// Import Swiper styles
+import "swiper/css";
+import Project from "./project";
 export default function Projects() {
   return (
-    <div className="relative flex justify-center h-screen">
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
+      className="relative flex justify-center h-screen"
+    >
       <h2 className="absolute md:top-20 top-8 tracking-[20px] uppercase my-5">
         PROJECTS
       </h2>
-      <Project />
-      <ScrollToTopBtn />
-    </div>
+      <Swiper
+        className="absolute md:top-36 top-32 my-5 select-none text-white"
+        loop={true}
+      >
+        <SwiperSlide>
+          <Project
+            img="https://appscrip.com/wp-content/uploads/2021/09/Instagram-clone-application-885x1024.png"
+            title="Instagram Clone"
+            description="React js - Bootstrap5"
+            demoUrl="https://instagram-clone-g6yxm3y8y-marziehs-projects.vercel.app"
+            githubUrl="https://jsonserver-instagram-clone.iran.liara.ru"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Project
+            img="https://appscrip.com/wp-content/uploads/2021/09/Instagram-clone-application-885x1024.png"
+            title="FullStack CMS"
+            description="Next js - Tailwind css -Mongo db"
+            demoUrl="https://instagram-clone-g6yxm3y8y-marziehs-projects.vercel.app"
+            githubUrl="https://jsonserver-instagram-clone.iran.liara.ru"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Project
+            img="https://appscrip.com/wp-content/uploads/2021/09/Instagram-clone-application-885x1024.png"
+            title="Real Estate"
+            description="Next js -Tailwind css - swiper - Mongo db"
+            demoUrl="https://instagram-clone-g6yxm3y8y-marziehs-projects.vercel.app"
+            githubUrl="https://jsonserver-instagram-clone.iran.liara.ru"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Project
+            img="https://appscrip.com/wp-content/uploads/2021/09/Instagram-clone-application-885x1024.png"
+            title="wikipedia"
+            description="React js - bootstrap5"
+            demoUrl="https://instagram-clone-g6yxm3y8y-marziehs-projects.vercel.app"
+            githubUrl="https://jsonserver-instagram-clone.iran.liara.ru"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Project
+            img="https://appscrip.com/wp-content/uploads/2021/09/Instagram-clone-application-885x1024.png"
+            title="CMS"
+            description="React js - Typescript"
+            demoUrl="https://instagram-clone-g6yxm3y8y-marziehs-projects.vercel.app"
+            githubUrl="https://jsonserver-instagram-clone.iran.liara.ru"
+          />
+        </SwiperSlide>
+      </Swiper>
+    </motion.div>
   );
 }
--- components/project.tsx
+++ components/project.tsx
@@ -1,35 +1,44 @@
+import { motion } from "framer-motion";
 import Link from "next/link";
 
-type Props = {};
+type Props = {
+  img: string;
+  demoUrl: string;
+  githubUrl: string;
+  title: string;
+  description: string;
+};
 
 import { FaCode, FaGithub } from "react-icons/fa";
-const Project = (props: Props) => {
+const Project = ({ img, title, description, demoUrl, githubUrl }: Props) => {
   return (
-    <div className="absolute md:top-36 top-20 flex flex-col md:justify-center items-center">
-      <img
-        src="https://appscrip.com/wp-content/uploads/2021/09/Instagram-clone-application-885x1024.png"
-        alt="w"
+    <div className="flex flex-col md:justify-center items-center">
+      <motion.img
+        initial={{ y: -200, opacity: 0 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1.2 }}
+        viewport={{ once: true }}
+        src={img}
+        alt="project image"
         className="object-contain w-52 h-52"
       />
-      <h2 className="my-4 font-semibold text-2xl text-slate-300">
-        Instagram Clone
-      </h2>
-      <p className="max-w-md leading-9">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis soluta
-        hic, quo, officiis maxime similique porro ipsum non, quam at velit
-        consectetur maiores voluptatem! Magni natus aliquam incidunt non beatae?
-      </p>
-      <div className="flex justify-center items-center gap-5">
-        <Link href="https://instagram-clone-g6yxm3y8y-marziehs-projects.vercel.app/">
-          <FaCode />
-        </Link>
-        <Link
-          href="https://jsonserver-instagram-clone.iran.liara.run"
-          className="link hover:text-white"
-        >
-          <FaGithub />
-        </Link>
-      </div>
+      <motion.div
+        initial={{ y: 200, opacity: 0 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1.2 }}
+        viewport={{ once: true }}
+      >
+        <h2 className="my-4 font-semibold text-2xl text-slate-300">{title}</h2>
+        <p className="max-w-md leading-9">{description}</p>
+        <div className="flex justify-center items-center gap-5">
+          <Link href={demoUrl}>
+            <FaCode />
+          </Link>
+          <Link href={githubUrl} className="link hover:text-white">
+            <FaGithub />
+          </Link>
+        </div>
+      </motion.div>
     </div>
   );
 };

97662f6b0cc3ee5c30ca670febc842d3c8afd1ea
marzieh <marziehmonshizade@yahoo.com>
2024-01-13 01:56:15 +0330

    coding message model

---
+++ models/message.js
@@ -0,0 +1,33 @@
+// message is entity
+const { default: mongoose } = require("mongoose");
+
+const schema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 50,
+    uppercase: true,
+  },
+  email: {
+    type: String,
+    match: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+    required: true,
+  },
+  subject: {
+    type: String,
+    minLength: 3,
+    maxLength: 100,
+    required: true,
+  },
+  body: {
+    type: String,
+    minLength: 3,
+    maxLength: 500,
+    required: true,
+  },
+});
+
+//have error
+const model = mongoose.models.message || mongoose.model("message", schema);
+export default model;

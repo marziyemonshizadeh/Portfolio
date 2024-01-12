7378edb1ba65aba080ae6aa9e0ae5d2f498fd0ac
marzieh <marziehmonshizade@yahoo.com>
2024-01-13 02:01:05 +0330

    Add getStaticProps function to index.js

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,14 +4,16 @@ import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
 import Hero from "@/components/hero";
 import Navbar from "@/components/navbar";
+import ScrollToTopBtn from "@/components/scrollToTopBtn";
+const { default: connectToDB } = require("@/utils/db");
 
 export default function Home() {
   return (
-    <main className="bg-zinc-800 text-zinc-500 text-lg snap-y snap-mandatory overflow-y-scroll overflow-x-hidden h-screen scrollbar-thin scrollbar-thumb-pink-500 scrollbar-track-gray-400/20 z-0">
+    <main className="static  bg-zinc-800 text-zinc-500 text-lg snap-y snap-mandatory overflow-y-scroll overflow-x-hidden h-screen scrollbar-thin scrollbar-thumb-pink-500 scrollbar-track-gray-400/20 z-0">
       {/* navbar */}
       <Navbar />
+      <ScrollToTopBtn />
       {/* hero */}
-      {/* <section id="hero" className="snap-center"> */}
       <section id="hero" className="snap-start h-screen">
         <Hero />
       </section>
@@ -33,4 +35,16 @@ export default function Home() {
       </section>
     </main>
   );
+}
+export async function getStaticProps() {
+  // first connect to db
+  connectToDB();
+  // const messages = await MessageModel.find({});
+  // console.log("messages = ", messages);
+  return {
+    props: {
+      // messages: JSON.parse(JSON.stringify(messages)),
+    },
+    revalidate: 60 * 60 * 12,
+  };
 }

56359c0d0c8607d12bc06faa8f3994ab5fc8702b
marzieh <marziehmonshizade@yahoo.com>
2024-01-18 16:02:32 +0330

    create typing file

---
+++ typings.d.ts
@@ -0,0 +1,32 @@
+import { Key } from "react";
+
+export interface myInfo{
+    _id:any,
+    name:string,
+    heroPageImgUrl:string,
+    aboutMePageImgUrl:string,
+    description:string,
+    phoneNumber:Number,
+    mail:String,
+    address:String
+}
+export interface FormValues{
+    name:string,
+    email:string,
+    subject:string,
+    body:string
+}
+export interface project{
+    _id: Key | null | undefined;
+    img:string,
+    description:string,
+    title:string,
+    demoUrl:string,
+    githubUrl:string
+}
+export interface skill{
+    _id: Key | null | undefined;
+    directionLeft:any;
+    imgUrl:string,
+    Percent:string
+}

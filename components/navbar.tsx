6e153fd11f6b5453a8f6a4fbdd2214c29bb31918
marzieh <marziehmonshizade@yahoo.com>
2024-03-02 02:49:25 +0330

    Add a language change button to the navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,14 +3,17 @@ import Link from "next/link";
 import {
   FaEnvelope,
   FaGithub,
-  FaInstagram,
   FaLinkedinIn,
   FaTelegramPlane,
 } from "react-icons/fa";
 
+import i18n from "@/i18n";
 import ThemeSwitch from "./themeSwitch";
 
 function Navbar() {
+  const changeLanguage = (event: { target: { value: string | undefined } }) => {
+    i18n.changeLanguage(event.target.value);
+  };
   return (
     <nav className="flex md:justify-around justify-between items-start md:p-3 p-2 sticky top-0 z-50">
       <motion.section
@@ -19,30 +22,28 @@ function Navbar() {
         transition={{ duration: 1.5 }}
         className="flex flex-row gap-2"
       >
+        {/* telegrm */}
         <Link
           href="https:/t.me/Marzieh_monshizadeh"
           className="link hover:text-cyan-500"
         >
           <FaTelegramPlane />
         </Link>
-        <Link
-          href="https://www.instagram.com/marzieh___monshizadeh/"
-          className="link hover:text-pink-500"
-        >
-          <FaInstagram />
-        </Link>
+        {/* Linkedin */}
         <Link
           href="https://www.linkedin.com/in/marzieh-monshizadeh-175a061a0/"
           className="link hover:text-sky-700"
         >
           <FaLinkedinIn />
         </Link>
+        {/* Github */}
         <Link
           href="https://github.com/marziyemonshizadeh/"
           className="link dark:hover:text-white hover:text-black"
         >
           <FaGithub />
         </Link>
+        {/* mail */}
         <Link href="mailto:marziehmonshizade@yahoo.com" className="link">
           <FaEnvelope />
         </Link>
@@ -53,6 +54,16 @@ function Navbar() {
         transition={{ duration: 1.5 }}
         className="flex items-center gap-3"
       >
+        <select
+          className="appearance-none p-1 rounded-full"
+          aria-label=".form-select-sm"
+          onChange={changeLanguage}
+        >
+          <option value="en" selected>
+            En
+          </option>
+          <option value="fa">fa</option>
+        </select>
         <Link href="#">
           <ThemeSwitch />
         </Link>

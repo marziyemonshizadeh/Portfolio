6d1544016f3a5691528baa71040d346e44a498c0
marzieh <marziehmonshizade@yahoo.com>
2024-03-29 17:09:25 +0330

    delete extra codes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,20 +2,10 @@ import Providers from "@/components/provider";
 import "@/styles/globals.css";
 import { useTheme } from "next-themes";
 import type { AppProps } from "next/app";
-import { useEffect, useState } from "react";
 import { BreakpointProvider } from "react-socks";
 
 export default function App({ Component, pageProps }: AppProps) {
   const { resolvedTheme } = useTheme();
-  const [showChild, setShowChild] = useState(false);
-
-  useEffect(() => {
-    setShowChild(true);
-  }, []);
-
-  if (!showChild) {
-    return null;
-  }
   return (
     <BreakpointProvider>
       <div

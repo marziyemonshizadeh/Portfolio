0b3c59f6bf2a6d8153a9990145331c80e18ebb69
marzieh <marziehmonshizade@yahoo.com>
2024-04-09 02:17:58 +0330

    deploy mongo db on liara and use liara url

--- utils/db.ts
+++ utils/db.ts
@@ -4,19 +4,11 @@ const connectToDB = async () => {
   try {
     // if connected
     if (mongoose.connections[0].readyState) {
-      console.log("connected az ghabl :)");
       return false;
     } else {
-      try {
-        await mongoose.connect("mongodb://localhost:27017/portfolio");
-        console.log("connected to db");
-        
-      } catch (err) {
-        console.log("have error to connect to db");       
-        console.log('error: ' + err)
-      }
-      // await mongoose.connect("mongodb://localhost:27017/portfolio");
-      // console.log("connected to db successfully :)");
+        await mongoose.connect(process.env.MONGODB_URI);
+        // await mongoose.connect("mongodb://root:kmz6RjjoZ4AWZFfNVucDabsM@monte-rosa.liara.cloud:30604/portfolio?authSource=admin");
+        // await mongoose.connect("mongodb://0.0.0.0/portfolio");
     }
   } catch (err) {
     console.log("connectToDB err => ", err);

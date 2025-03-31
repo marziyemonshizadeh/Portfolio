2a3446f985eda93ace4f8057040c7f5bb8192bcb
marzieh <marziehmonshizade@yahoo.com>
2025-03-31 16:44:21 +0330

    Fix: change mongodb url

--- utils/db.ts
+++ utils/db.ts
@@ -6,9 +6,8 @@ const connectToDB = async () => {
     if (mongoose.connections[0].readyState) {
       return false;
     } else {
-        await mongoose.connect(process.env.MONGODB_URI);
-        // await mongoose.connect("mongodb://root:kmz6RjjoZ4AWZFfNVucDabsM@monte-rosa.liara.cloud:30604/portfolio?authSource=admin");
-        // await mongoose.connect("mongodb://0.0.0.0/portfolio");
+      // await mongoose.connect(process.env.MONGODB_URI);
+      await mongoose.connect(process.env.MONGODB_URI2);
     }
   } catch (err) {
     console.log("connectToDB err => ", err);
